bd86abadfe08fba788d8cc14edbe8d2241d20227
jakubvlk <jakvlk@seznam.cz>
2013-11-23 14:26:45 +0100

    upload projektu

---
+++ Fotki/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fotki
+//
+//  Created by maly Kuba on 23/11/13.
+//  Copyright (c) 2013 Craneballs_Barcelona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fotki/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Fotki
+//
+//  Created by maly Kuba on 23/11/13.
+//  Copyright (c) 2013 Craneballs_Barcelona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
